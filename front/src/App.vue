9e7ad01bd2be24357db4ce8fcc31b77ccae5f4eb
azusaw <azusa0304@gmail.com>
2023-02-09 02:22:08 +0200

    notification

--- front/src/App.vue
+++ front/src/App.vue
@@ -12,7 +12,17 @@ export default {
         { name: "Summary", path: "/summary/" },
         { name: "Chat", path: "/chat/" },
       ],
+      drawer: false,
+      middleSize: ["hidden-all", "lg", "xl"],
     };
+  },
+  methods: {
+    openDrawer() {
+      this.drawer = true;
+    },
+    closeDrawer() {
+      this.drawer = false;
+    },
   },
 };
 </script>
@@ -21,7 +31,7 @@ export default {
   <el-container class="container">
     <el-header class="header">
       <LogoSvg />
-      <nav class="nav">
+      <nav class="pc-nav">
         <RouterLink
           class="nav-item"
           v-for="item in pages"
@@ -30,37 +40,78 @@ export default {
           >{{ item.name }}</RouterLink
         >
       </nav>
+      <div class="mobile-nav" :onclick="openDrawer">Menu</div>
+      <el-drawer v-model="drawer" size="300">
+        <nav>
+          <RouterLink
+            class="drawer-nav-item"
+            v-for="item in pages"
+            :key="item.name"
+            :to="item.path"
+            :onclick="closeDrawer"
+            >{{ item.name }}</RouterLink
+          >
+        </nav>
+      </el-drawer>
     </el-header>
     <el-main class="main"><RouterView /></el-main>
     <!-- hide footer in chat page -->
-    <el-footer class="footer" v-show="this.$route.path !== '/chat'">
+    <el-footer class="footer" v-show="this.$route.path !== '/chat/'">
       Copyright {{ new Date().getFullYear() }} azusaw All rights reserved.
     </el-footer>
   </el-container>
 </template>
 
 <style scoped>
+.container {
+  margin: 0 auto;
+  padding: 0;
+  max-width: 800px;
+}
+
+@media screen and (max-width: 800px) {
+  .container {
+    max-width: 100vw;
+  }
+}
+
 .header {
   position: sticky;
   top: 0;
   display: flex;
   align-items: center;
   height: var(--header-height);
-  padding-left: 10px;
+  padding: 10px;
   background-color: var(--primary-color);
   color: white;
   z-index: 10;
 }
 
-.nav {
+.pc-nav,
+.mobile-nav {
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: end;
 }
 
-.nav-item {
-  margin: 10px;
+/* apply when screen width is smaller than 660px */
+@media screen and (max-width: 660px) {
+  .pc-nav {
+    display: none;
+  }
+}
+
+/* apply when screen width is wider than 661px */
+@media screen and (min-width: 661px) {
+  .mobile-nav {
+    display: none;
+  }
+}
+
+.mobile-nav,
+.nav-item,
+.drawer-nav-item {
   padding: 10px;
   border-radius: 10px;
   font-weight: 600;
@@ -74,9 +125,15 @@ export default {
   transition: 0.5s;
 }
 
+.drawer-nav-item {
+  display: block;
+  color: #444;
+}
+
 .main {
   min-height: calc(100vh - 120px);
-  padding: 20px;
+  padding: 20px 10px;
+  max-width: 100vw;
 }
 
 .footer {
